9c0b0ed1615c372f1653b4bd3bf05848e9453601
deviantdes <des-@live.com.sg>
2017-02-06 13:48:32 +0800

    intial

---
+++ MvcAngular2/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template : "<h1>{{welcomeMessage}}</h1>"
+
+})
+
+export class AppComponent
+{
+    welcomeMessage: String = "My First ng2";
+}   
---
+++ MvcAngular2/app/app.module.ts
@@ -0,0 +1,3 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
